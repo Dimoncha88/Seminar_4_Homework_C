d3ebe9e9301e876c0b4791604656604eec9cd22a
Dmitry <dimonch_rus@mail.ru>
2022-12-05 18:55:02 +0300

    add Task 03

---
+++ Task03/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из N элементов 
+// и выводит их на экран.
+
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+Console.Clear();
+Console.WriteLine("Введите размерность массива:");
+int n = int.Parse(Console.ReadLine());
+int[] RandArray(int m)
+{
+    Random rnd = new Random();
+    int[] arr = new int[m];
+    for(int i = 0; i < m; i++)
+    {
+        arr[i] = rnd.Next(0,100);
+    }
+    return arr;
+}
+
+int[] arr = RandArray(n);
+Console.Write("[");
+Console.Write(String.Join(", ", arr));
+Console.Write("]");
